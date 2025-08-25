d2a0d7c59574ec4d9c3e396620fe8fba6e32936a
Slava_Moiseev <qualityengineerinkom@gmail.com>
2025-08-25 20:43:51 +0300

    done

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,19 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore countSeats;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.countSeats = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        if (countSeats.tryAcquire()) {
+            return new BookingResult(user, true, "Booking successful.");
+        } else {
+            return new BookingResult(user, false, "No seats available.");
+        }
     }
 }
